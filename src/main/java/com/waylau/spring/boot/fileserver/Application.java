867ff39c8b47b90f7903a7f50df8fdd8021c1972
dengji85 <dengji85@gmail.com>
2017-12-08 18:03:13 +0800

    添加eureka，还未实现

--- src/main/java/com/waylau/spring/boot/fileserver/Application.java
+++ src/main/java/com/waylau/spring/boot/fileserver/Application.java
@@ -1,12 +1,11 @@
 package com.waylau.spring.boot.fileserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
